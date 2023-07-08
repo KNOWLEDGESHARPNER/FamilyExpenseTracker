892e464d4aed3033812c8741d890dc351c640779
KNOWLEDGESHARPNER <ashubig75@gmail.com>
2023-07-08 11:32:44 +0530

    FamilyMember admin is registered

--- familymembers/admin.py
+++ familymembers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+import familymembers
+
+
 # Register your models here.
+@admin.register(familymembers)
+class FamilyMemberAdmin(admin.ModelAdmin):
+    search_fields = ['name']
