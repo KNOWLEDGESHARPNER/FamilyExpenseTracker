dcc765e1d39cc60d4e9128f590cf4f5a7542a2aa
KNOWLEDGESHARPNER <ashubig75@gmail.com>
2023-07-10 10:07:25 +0530

    admin.py changed

--- familymembers/admin.py
+++ familymembers/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
 
-import familymembers
 
 
-# Register your models here.
-@admin.register(familymembers)
-class FamilyMemberAdmin(admin.ModelAdmin):
-    search_fields = ['name']
