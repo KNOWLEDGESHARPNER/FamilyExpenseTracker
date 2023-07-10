f34d55a579402ae8700a1ea87a409be2ca82df05
KNOWLEDGESHARPNER <ashubig75@gmail.com>
2023-07-10 17:33:36 +0530

    configured whitenoise

--- familymembers/models.py
+++ familymembers/models.py
@@ -11,6 +11,6 @@ class FamilyMembers(models.Model):
         return self.name
 
 class Expenses(models.Model):
-    membername=models.ForeignKey(FamilyMembers)
+    membername=models.ForeignKey(FamilyMembers,on_delete=models.CASCADE)
     expense=models.IntegerField(default=0)
     purpose = models.TextField(default=None,max_length=120)
