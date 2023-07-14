0d0948f8aebb464b663135cf83485fce27d68f46
KNOWLEDGESHARPNER <ashubig75@gmail.com>
2023-07-14 17:11:02 +0530

    static root path configured

--- FamilyExpenseTracker/settings.py
+++ FamilyExpenseTracker/settings.py
@@ -139,6 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), # Django will search for /src/
 ]
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
