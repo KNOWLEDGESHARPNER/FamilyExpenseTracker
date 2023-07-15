26099e8d8d4ab7152c69e8f602a625a4520c6c8e
KNOWLEDGESHARPNER <ashubig75@gmail.com>
2023-07-15 09:59:08 +0530

    Merge branch 'master' of https://github.com/KNOWLEDGESHARPNER/FamilyExpenseTracker

--- FamilyExpenseTracker/settings.py
+++ FamilyExpenseTracker/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"), # Django will search for /src/
+    os.path.join(BASE_DIR, "staticfiles"), # Django will search for /src/
 ]
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
